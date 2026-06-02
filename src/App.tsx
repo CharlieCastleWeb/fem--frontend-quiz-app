45b77ad453b31d4c844e8ddf4898d1989a12562d
Carlos Castillo Martinez <castillomartinezcarlos@gmail.com>
2026-06-02 07:15:00 +0200

    feat: main page background dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,10 @@ import ThemeToggle from "./components/theme-toggle";
 
 function App() {
   return (
-    <div className="bg-fem-gray-50 bg-mobile-light min-h-screen min-w-screen">
+    <div
+      className="bg-fem-gray-50 bg-mobile-light-pattern h-screen w-full
+    dark:bg-fem-blue-900 dark:bg-mobile-dark-pattern duration-300 ease-in-out"
+    >
       <header className="py-4 px-6 flex justify-end items-center">
         <ThemeToggle></ThemeToggle>
       </header>
