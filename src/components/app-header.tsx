295be6c2eeb37308f2f60dcd5f243d3ad92b2170
Carlos Castillo Martinez <castillomartinezcarlos@gmail.com>
2026-06-24 15:54:35 +0200

    fix: header height

--- src/components/app-header.tsx
+++ src/components/app-header.tsx
@@ -8,7 +8,7 @@ type AppHeaderProps = {
 
 export default function AppHeader({ selectedSubject }: AppHeaderProps) {
   return (
-    <header className="h-18 py-4 flex justify-between items-center md:py-10 md:h-auto lg:py-21 lg:max-w-290 lg:mx-auto lg:px-0">
+    <header className="h-18 py-4 flex justify-between items-center md:py-10 md:h-10 lg:py-21 lg:h-14 lg:max-w-290 lg:mx-auto lg:px-0">
       <div className="min-w-0">
         {selectedSubject && (
           <SubjectBadge subject={selectedSubject}></SubjectBadge>
