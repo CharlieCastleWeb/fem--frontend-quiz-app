15fec2b51deb0af98445548d189a2c31a333904f
Carlos Castillo Martinez <castillomartinezcarlos@gmail.com>
2026-06-25 07:28:13 +0200

    feat: add GitHub Pages deployment workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/fem--frontend-quiz-app/",
   plugins: [react(), tailwindcss()],
 });
